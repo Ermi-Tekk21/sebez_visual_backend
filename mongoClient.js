e4c743ba04468b6ba1bff3b7018c0c63902df7d8
Ermias-TekileMarkos <ermaatekk@gmail.com>
2024-07-22 12:33:30 +0300

    some update...

---
+++ mongoClient.js
@@ -0,0 +1,25 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const uri = process.env.MONGO_URI;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    await client.close();
+  }
+}
+
+run().catch(console.dir);
+
+module.exports = client;
--- server.js
+++ server.js
@@ -1,34 +1,25 @@
-const express = require("express"); // Imports the Express framework.
-const mongoose = require("mongoose"); //Imports Mongoose, an ODM (Object Data Modeling) library for MongoDB.
-const dotenv = require("dotenv"); //Imports dotenv to manage environment variables.
-const userRoute = require("./routes/userRoute"); // No need for .js extension with CommonJS
-const productRoute = require("./routes/productRouter"); // No need for .js extension with CommonJS
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/userRoute");
+const productRoute = require("./routes/productRouter");
 const { authenticateUser } = require("./controller/authenticateUser");
 const cors = require("cors");
 
-dotenv.config(); // Load environment variables from .env file
+dotenv.config();
 
 const app = express();
-
 const port = process.env.PORT || 3004;
-const mongoURI = process.env.MONGO_URI; // Use the MONGO_URI environment variable
+const mongoURI = process.env.MONGO_URI;
 
-// Built-in middleware for parsing JSON and URL-encoded bodies
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors()); // Enable CORS for all origins
+app.use(cors());
 
-// Use routes
 app.use("/api/user", userRoute);
 app.use("/api/auth", authenticateUser);
 app.use("/api/product", productRoute);
 
-// configration
-// console.log('application: ' + config.get('name'));
-// console.log('male server name: ' + config.get('mail.host'));
-// console.log('male password: ' + config.get('mail.password'));
-
-// Connect to MongoDB
 mongoose
   .connect(mongoURI)
   .then(() => {
